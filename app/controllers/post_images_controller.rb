f79691844e5f0dea03fdf4866447bc7d35467ae9
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-01-19 14:41:01 +0000

    chapter11 end

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,15 +10,18 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
+  
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
-
-  private
+  
+ private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
+  
 end
